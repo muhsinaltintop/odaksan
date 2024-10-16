caf5c16d8f83c0646099b90ce86a1c7f78fb0c09
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-10-16 13:56:50 +0100

    bugs fixed

--- app/_components/Slider.jsx
+++ app/_components/Slider.jsx
@@ -1,29 +1,35 @@
+
 "use client"
 import React, { useState, useEffect, useCallback } from 'react';
 import SingleSlide from './_atoms/SingleSlide';
 import { Arrow } from './_atoms/Icons';
-
 const SliderComponent = () => {
-  const slides = [<SingleSlide key={0} />, <SingleSlide key={1} />, <SingleSlide key={2} />]; // Slider için slide'lar
+  // Slide verilerini bir array olarak tutun.
+  const slideData = [
+    { id: 1, content: 'Slide 1 content' },
+    { id: 2, content: 'Slide 2 content' },
+    { id: 3, content: 'Slide 3 content' }
+  ];
+
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  // Bir sonraki slide'a geçiş fonksiyonu, useCallback ile sarıldı
+  // Bir sonraki slide'a geçiş fonksiyonu
   const goToNextSlide = useCallback(() => {
-    setCurrentSlide((prev) => (prev + 1) % slides.length);
-  }, [slides.length]); // slides.length bağımlılık olarak ayarlandı
+    setCurrentSlide((prev) => (prev + 1) % slideData.length);
+  }, [slideData.length]);
 
-  // Bir önceki slide'a geçiş fonksiyonu, buna da isterseniz useCallback uygulanabilir
+  // Bir önceki slide'a geçiş fonksiyonu
   const goToPreviousSlide = () => {
-    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
+    setCurrentSlide((prev) => (prev - 1 + slideData.length) % slideData.length);
   };
 
   // Otomatik kaydırma için useEffect
   useEffect(() => {
     const interval = setInterval(() => {
-      goToNextSlide(); // Otomatik olarak bir sonraki slide'a geçiş
-    }, 5000); // 3 saniye bekleyerek slide değişimi
+      goToNextSlide();
+    }, 5000);
     return () => clearInterval(interval);
-  }, [goToNextSlide]); // useEffect bağımlılığı olarak goToNextSlide eklendi
+  }, [goToNextSlide]);
 
   return (
     <div className="relative w-full h-full overflow-hidden">
@@ -32,27 +38,27 @@ const SliderComponent = () => {
         className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-primary text-white px-4 py-2 z-10 rounded-full ml-2 text-xl font-bold"
         onClick={goToPreviousSlide}
       >
-        <Arrow flipped={true}/>
+        <Arrow flipped={true} />
       </button>
       <button
         className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-primary text-white px-4 py-2 z-10 rounded-full mr-2"
         onClick={goToNextSlide}
       >
-        <Arrow flipped={false}/>
+        <Arrow flipped={false} />
       </button>
 
       {/* Slide'ları göstermek için */}
       <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
-        {slides.map((slide, index) => (
-          <div key={index} className="min-w-full">
-            {slide}
+        {slideData.map((slide, index) => (
+          <div key={slide.id} className="min-w-full">
+            <SingleSlide content={slide.content} /> {/* Her slaytı render et */}
           </div>
         ))}
       </div>
 
       {/* Alt kısımda dot navigasyonu */}
       <div className="flex justify-center mt-4">
-        {slides.map((_, index) => (
+        {slideData.map((_, index) => (
           <div
             key={index}
             className={`w-3 h-3 rounded-full mx-2 cursor-pointer ${index === currentSlide ? 'bg-gray-800' : 'bg-gray-400'}`}
